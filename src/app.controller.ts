f379f6a164d5fe1853a0b5862715ccc1e84c3d41
Muhotasim Fuad <muhotasimf@gmail.com>
2023-12-16 11:38:44 +0600

    updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,11 +7,27 @@ export class AppController {
   @Get('/')
   @Render('index')
   root() {
-    return { message: 'Hello world!' };
+    return {
+      message: 'Hello world!',
+      seoData: {
+        title: 'Home',
+        meta_title: 'Home',
+        meta_description: '',
+        meta_keywords: '',
+      },
+    };
   }
   @Get('/*')
   @Render('dynamic-page')
   page() {
-    return { message: 'Pages' };
+    return {
+      message: 'Dynamic Page',
+      seoData: {
+        title: 'Dynamic Page',
+        meta_title: 'D p',
+        meta_description: '',
+        meta_keywords: '',
+      },
+    };
   }
 }
